ba30acf5c0b18ef1404d4a9e0521ec1b94035839
anupJag <38972668+anupJag@users.noreply.github.com>
2018-11-09 15:54:49 +0530

    Commit

---
+++ src/webparts/doctorsAppointment/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const footer = (props) => {
+    return(
+        <footer>
+            <div></div>
+            <div></div>
+        </footer>
+    );
+};
+
+export default footer;
--- src/webparts/doctorsAppointment/components/Header/Header.tsx
+++ src/webparts/doctorsAppointment/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 const header = (props) => {
     return(
         <header>
-
+            
         </header>
     );
 };
